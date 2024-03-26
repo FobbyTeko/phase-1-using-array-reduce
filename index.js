a48c8bfbfd114558ce4365469a98f0a0cfab3cb0
Tiri Tuatai <tiri.jnr@gmail.com>
2024-03-26 21:49:14 +0800

    reduce map done

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const initialBatteriesValue = 0;
+
+const totalBatteries = batteryBatches.reduce(
+    (accum, currentValue) => accum + currentValue,
+    initialBatteriesValue,
+);
+
+console.log(`Battery Amount: ${totalBatteries}`);
